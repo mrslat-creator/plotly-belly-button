f8e752c8b00d3f980754be33d4c1c08e23091913
Altricia Latimer <altricia.latimer@gmail.com>
2021-06-03 16:04:58 -0400

    Update app.js

---
+++ Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,25 @@
+//Function for change on dropdown menu
+function optionChanged(selectedID){
+
+    //Check if value is selcted in dropdown
+    console.log(selectedID);
+
+    // Read the json file for the data
+    d3.json("data/samples.json").then((data) =>){
+
+    // console.log(data);
+
+      //Clears dropdown
+      d3.select("#selDtaset").html("");
+
+    // Select the metadata array and for each item append the item ID and adds ID to dropdown
+    data.metadata.array.forEach(item=>)
+    {
+        //console.log(item.id);
+        d3.select ("#selDataset").node().value = selectedID;
+        
+    }
+        
+    });
+    }
+}
